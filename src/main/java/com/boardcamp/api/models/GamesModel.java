27fc2c1117426ded20d756fe208d54a4edc940f1
leonardoyusuke <leoyuhirano@gmail.com>
2024-02-06 11:34:47 +0900

    refact

--- src/main/java/com/boardcamp/api/models/GamesModel.java
+++ src/main/java/com/boardcamp/api/models/GamesModel.java
@@ -1,14 +1,11 @@
 package com.boardcamp.api.models;
 
-import com.boardcamp.api.dtos.CustomerDTO;
 import com.boardcamp.api.dtos.GamesDTO;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
